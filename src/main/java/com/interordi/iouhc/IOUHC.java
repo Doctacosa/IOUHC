96b6fe61136f179a72ee689a7266596eed9e7dd7
Stéphane Lemay <interordi@interordi.com>
2024-08-19 23:26:14 -0400

    Minor refactor

--- src/main/java/com/interordi/iouhc/IOUHC.java
+++ src/main/java/com/interordi/iouhc/IOUHC.java
@@ -8,6 +8,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.interordi.iouhc.utilities.Scores;
 
 
 public class IOUHC extends JavaPlugin {
--- src/main/java/com/interordi/iouhc/utilities/Scores.java
+++ src/main/java/com/interordi/iouhc/utilities/Scores.java
@@ -1,4 +1,4 @@
-package com.interordi.iouhc;
+package com.interordi.iouhc.utilities;
 
 import java.util.Comparator;
 import java.util.HashMap;
